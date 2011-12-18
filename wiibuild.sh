6c62df9ca9d5750e4045ef1a86b1be8ab624c115
Simone Renzo <simone201@gmail.com>
2011-12-18 13:37:42 +0100

    Added in-kernel cross-compilers + fixed wiibuild.sh

--- wiibuild.sh
+++ wiibuild.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 
-export LD_LIBRARY_PATH=/home/dudu/H-x86_64-pc-linux-gnu/cross-powerpc-linux-uclibc/usr/lib
+export LD_LIBRARY_PATH=H-x86_64-pc-linux-gnu/cross-powerpc-linux-uclibc/usr/lib
 
-make wii_defconfig ARCH=powerpc CROSS_COMPILE=/home/dudu/H-x86_64-pc-linux-gnu/cross-powerpc-linux-uclibc/usr/bin/powerpc-linux-
+make wii_defconfig ARCH=powerpc CROSS_COMPILE=H-x86_64-pc-linux-gnu/cross-powerpc-linux-uclibc/usr/bin/powerpc-linux-
 
-make -j2 ARCH=powerpc CROSS_COMPILE=/home/dudu/H-x86_64-pc-linux-gnu/cross-powerpc-linux-uclibc/usr/bin/powerpc-linux-
+make -j2 ARCH=powerpc CROSS_COMPILE=H-x86_64-pc-linux-gnu/cross-powerpc-linux-uclibc/usr/bin/powerpc-linux-
 
-find -name '*.ko' -exec cp -av {} ../moduli/ \;
+cp arch/powerpc/boot/zImage ./ppcboot.elf
+
+#find -name '*.ko' -exec cp -av {} ../moduli/ \;
